f1ed5fe550005dc74e8ef7bdc3286f4475c8df05
M.Mavromanolakis <mauromanolakism@gmail.com>
2024-01-21 22:16:54 +0200

    Main updated

--- src/gr/aueb/cf/Proj5/Main.java
+++ src/gr/aueb/cf/Proj5/Main.java
@@ -3,6 +3,6 @@ package gr.aueb.cf.Proj5;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Coding Factory!!!!!!");
+        System.out.println("Hello Coding Factory!!!!!!!!!!");
     }
 }
