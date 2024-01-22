6ebe369c29041cac531afe480da5b3297793a6be
M.Mavromanolakis <mauromanolakism@gmail.com>
2024-01-22 09:29:56 +0200

    Method sayHello

--- src/gr/aueb/cf/Proj5/Main.java
+++ src/gr/aueb/cf/Proj5/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Coding Factory!!!!!!!!!!");
+        sayHello();
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello to everyone!!!!");
     }
 }
